cc2c6c293d0c9b8c3a9fbfdff3e9cd46ee754615
shanzhongheshi <869060393@qq.com>
2022-05-03 17:13:52 +0800

    202220502 master -01

---
+++ java-se-ljx/src/com/ljx/collector/MapTest.java
@@ -0,0 +1,15 @@
+package com.ljx.collector;
+
+import org.junit.Test;
+
+/**
+ * 作用：Map的循环遍历
+ * @author Lin
+ * 2022/5/3  16:36
+ */
+public class MapTest {
+    @Test
+    public void test1(){
+
+    }
+}
