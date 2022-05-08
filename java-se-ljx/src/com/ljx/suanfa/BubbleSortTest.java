e1bb85b5d683b0c16fa6131bfd0d952004c6e685
shanzhongheshi <869060393@qq.com>
2022-05-08 22:38:28 +0800

    202220508 master -01

--- java-se-ljx/src/com/ljx/suanfa/BubbleSortTest.java
+++ java-se-ljx/src/com/ljx/suanfa/BubbleSortTest.java
@@ -7,8 +7,9 @@ package com.ljx.suanfa;
 public class BubbleSortTest {
     public static void main(String[] args) {
 
-        int[] arr = new int[]{43,32,76,-98,0,64,33,-21,32,99};
-
+        int[] arr = new int[]{43,32,76,-98,99,0,64,33,-21,32};
+        System.out.println("排序之前：\n"+java.util.Arrays.toString(arr));
+        System.out.println("开始排序：");
         //冒泡排序
         for(int i = 0;i < arr.length - 1;i++){
 
@@ -21,13 +22,13 @@ public class BubbleSortTest {
                 }
 
             }
-
+            System.out.println(java.util.Arrays.toString(arr));
         }
+        System.out.println("排序之后：\n"+java.util.Arrays.toString(arr));
 
-
-        for(int i = 0;i < arr.length;i++){
-            System.out.print(arr[i] + "\t");
-        }
+//        for(int i = 0;i < arr.length;i++){
+//            System.out.print(arr[i] + "\t");
+//        }
 
     }
 }
